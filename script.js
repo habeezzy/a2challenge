91c2568baaaa44fa3920fa286c4bf1c6664c020f
habeezzy <mhabeebulah@gmail.com>
2017-11-03 12:25:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var user {
+	email: "coolguy@gmail.com",
+	password: "IloveCoffee",
+	name: "Grant Chirpus"
+},
+
+
+function logIn()
+function checkLogin("login") {
+  if (login === true){
+    display
+  }
+
+} else {
+	alert.user("Sign-in credentials are incorrect");
+}
+
+
